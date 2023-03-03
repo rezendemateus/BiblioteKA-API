58d4c93feb9049db8083d705b7905de828f66aff
Thomas Schreiner <iteano.thom@hotmail.com>
2023-03-03 15:44:19 -0300

    feat: add copy app with models

---
+++ copies/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Copy(models.Model):
+    avaliable = models.BooleanField(default=True)
+    book = models.ForeignKey(
+        "books.Book",
+        on_delete=models.CASCADE,
+        related_name="copies",
+    )
+
+    borrowed_to_users = models.ManyToManyField(
+        "users.User", through="copies.Loan", related_name="Copies"
+    )
+
+
+class Loan(models.Model):
+    borrowed_at = models.DateTimeField(auto_now_add=True)
+    loan_term_at = models.DateTimeField()
+    amount_paid = models.IntegerField(null=True)
+    paid_at = models.DateTimeField(null=True)
+
+    user_id = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    copy_id = models.ForeignKey("copies.Copy", on_delete=models.CASCADE)
