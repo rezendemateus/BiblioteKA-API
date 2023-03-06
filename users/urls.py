cb82c6e97f4f85b1efa7f351eeda8183ef898f5e
Juliana Tissiani Diorio <julianatdiorio@gmail.com>
2023-03-06 14:31:17 -0300

    fix: edit urls users

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path("users/", views.UserView.as_view()),
     path("users/:users_id/", views.UserDetailView.as_view()),
     path("users/login/", jwt_views.TokenObtainPairView.as_view()),
-    # path("users/:users_id/books", .as_view()),
     # path("users/:users_id/loan/historic", .as_view()),
 ]
