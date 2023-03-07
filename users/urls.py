3ef2c9d714d9f4cd2a704e1a814d19ab9a5da461
Juliana Tissiani Diorio <julianatdiorio@gmail.com>
2023-03-07 10:31:12 -0300

    feat: create serializers, views and permissions of users

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path("users/", views.UserView.as_view()),
-    path("users/:users_id/", views.UserDetailView.as_view()),
+    path("users/<int:user_id>/", views.UserDetailView.as_view()),
     path("users/login/", jwt_views.TokenObtainPairView.as_view()),
     # path("users/:users_id/loan/historic", .as_view()),
 ]
