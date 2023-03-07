2cff024fe9f61210b62513bc8351804c1842497f
Juliana Tissiani Diorio <julianatdiorio@gmail.com>
2023-03-07 20:12:38 -0300

    fix: removed doc swagger

--- _BiblioteKA/urls.py
+++ _BiblioteKA/urls.py
@@ -27,11 +27,6 @@ urlpatterns = [
     path("api/", include("books.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/docs/swagger/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger",
-    ),
-    path(
         "api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
     ),
 ]
