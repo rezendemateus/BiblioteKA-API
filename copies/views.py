d693ecaad6c73122d86c61f7b807d735b5a9bc3c
Juliana Tissiani Diorio <julianatdiorio@gmail.com>
2023-03-10 13:55:33 -0300

    feat: create send email

--- copies/views.py
+++ copies/views.py
@@ -4,13 +4,15 @@ from rest_framework.generics import (
     UpdateAPIView,
 )
 from .models import Loan, Copy
-from books.models import Book
+from books.models import Book, Follower
 from users.models import User
 from .serializers import LoanSerializer
 from books.permissions import IsAdminOrReadOnly
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.exceptions import ParseError, NotFound
 from .permissions import IsUserUnblocked
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 class LoanView(CreateAPIView):
@@ -23,7 +25,6 @@ class LoanView(CreateAPIView):
     def perform_create(self, serializer):
         book = get_object_or_404(Book, pk=self.kwargs.get("book_id"))
         user = get_object_or_404(User, username=self.request.data["username"])
-
         copy = Copy.objects.filter(book=book, avaliable=True).first()
 
         if not copy:
@@ -44,8 +45,27 @@ class LoanDetailView(UpdateAPIView):
     lookup_url_kwarg = "loan_id"
 
     def get_object(self):
+        # import ipdb
+
         loan = Loan.objects.filter(id=self.kwargs.get("loan_id")).first()
+        followers = loan.copy.book.followers.all()
+        followers = [follower.email for follower in followers]
+        copy = loan.copy.avaliable
+
+        # ipdb.set_trace()
+
         if not loan:
             raise NotFound("Loan does not exist")
+
+        print(copy is True, "---------------------------->")
+        if copy is True:
+            send_mail(
+                subject="Teste de envio de email",
+                message="Não foi um fracasso",
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=followers,
+                fail_silently=False,
+            )
+            print("Email enviado")
 
         return loan
