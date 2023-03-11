10b3203744a345344291a1a59985ba59a528505a
Juliana Tissiani Diorio <julianatdiorio@gmail.com>
2023-03-11 18:46:47 -0300

    fix: create charge return

--- copies/reminder_devolution.py
+++ copies/reminder_devolution.py
@@ -11,9 +11,10 @@ def reminder_devolution():
     loans = Loan.objects.all()
 
     for loan in loans:
-        reminder_day = loan.loan_term_at - timedelta(days=1)
+        reminder_day = loan.loan_term_at
         user_email = loan.user.email
-        if reminder_day.strftime("%d, %b, %Y") is timezone.now().strftime("%d, %b, %Y"):
+        day_before = timezone.now().strftime("%d, %b, %Y") - timedelta(days=1)
+        if reminder_day.strftime("%d, %b, %Y") is day_before:
             send_mail(
                 subject="ATENÇÃO: Lembrete de devolução!",
                 message="O livro que você emprestou"
@@ -23,7 +24,19 @@ def reminder_devolution():
                 recipient_list=[user_email],
                 fail_silently=False,
             )
-            print("roda a roda jequiti")
+
+        elif reminder_day.strftime("%d, %b, %Y") < timezone.now().strftime(
+            "%d, %b, %Y"
+        ):
+            send_mail(
+                subject="ATENÇÃO: Você possui livro(s) em atraso!",
+                message="O livro que você emprestou"
+                f' "{loan.copy.book.title}" '
+                "já deveria ter sido devolvido! Lembre-se: há multa por atraso na devolução e você pode renovar o empréstimo!",
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[user_email],
+                fail_silently=False,
+            )
 
 
 schedule.every().day.at("10:00").do(reminder_devolution)
