e974929a0aa9eebc20b30f882e85638170d0ed77
Juliana Tissiani Diorio <julianatdiorio@gmail.com>
2023-03-11 16:44:31 -0300

    feat: create function of reminder devolution with schedule

--- copies/views.py
+++ copies/views.py
@@ -13,6 +13,9 @@ from rest_framework.exceptions import ParseError, NotFound
 from .permissions import IsUserUnblocked
 from django.core.mail import send_mail
 from django.conf import settings
+from datetime import timedelta
+import schedule
+import time
 
 
 class LoanView(CreateAPIView):
@@ -65,3 +68,29 @@ class LoanDetailView(UpdateAPIView):
             )
 
         return loan
+
+
+def reminder_devolution():
+    loans = Loan.objects.all()
+
+    for loan in loans:
+        reminder_day = loan.loan_term_at - timedelta(days=1)
+        user_email = loan.user.email
+
+        if reminder_day is loan.loan_term_at - timedelta(days=1):
+            send_mail(
+                subject="ATENÇÃO: Lembrete de devolução!",
+                message="O livro que você emprestou"
+                f' "{loan.copy.book.title}" '
+                "deve ser devolvido até amanhã! Lembre-se: há taxa de multa caso não seja devolvido no dia, e esse valor é atualizado diariamente! Caso precise de mais tempo, renove seu empréstimo!",
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[user_email],
+                fail_silently=False,
+            )
+
+
+schedule.every().day.at("10:00").do(reminder_devolution)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
